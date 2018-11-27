86a20b12596fff0eecfc34608be36800a940a923
Wutyi <38199665+KhaingWutyiHnin@users.noreply.github.com>
2018-11-27 09:51:24 +0630

    first commit

---
+++ testing.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Repo Testing Plugin
+Description: testing site plugin
+Plugin URI: https://github.com/KhaingWutyiHnin/testing.git
+Version: 0.1b
+Text Domain: kwyhtest
+Domain Path: /languages
+    Copyright (C) 2018 Digital Agency Corp.
+*/
